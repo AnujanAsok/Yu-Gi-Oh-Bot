0b74cedc524502aee4b5a26c0420d096a63dabdf
AnujanAsok <udns.spartan@gmail.com>
2020-10-27 19:22:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -117,6 +117,8 @@ const drawCommand = (message) => {
   });
 };
 
+const inventoryCommand = () => {};
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.send("Hello from Render!"));
