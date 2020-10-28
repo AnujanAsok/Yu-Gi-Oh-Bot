23e32b6d03f2242f91dbd18d42b59afacd0a5325
AnujanAsok <udns.spartan@gmail.com>
2020-10-28 01:15:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-const localenv = require("./localenv.js");
-
-const BOT_TOKEN = process.env.BOT_TOKEN || localenv.BOT_TOKEN;
+const BOT_TOKEN = process.env.BOT_TOKEN || require("./localenv.js").BOT_TOKEN;
 exports.BOT_TOKEN = BOT_TOKEN;
